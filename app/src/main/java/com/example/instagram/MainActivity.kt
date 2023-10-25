87e25884e0cd16e980c0be192d44449943a1022b
anderodriguesilva <rodriguesilva.anderson@gmail.com>
2023-10-25 17:02:10 -0300

    Criada a Toolbar

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -3,22 +3,16 @@ package com.example.instagram
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.instagram.ui.theme.InstagramTheme
+import com.example.instagram.ui.view.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             InstagramTheme {
-
-                }
+                HomeScreen()
             }
         }
     }
+}
