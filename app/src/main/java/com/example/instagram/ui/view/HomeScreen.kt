98fa6fcc8281116b7bfcab9e1240a9f9d91716d0
anderodriguesilva <rodriguesilva.anderson@gmail.com>
2023-10-26 14:44:45 -0300

    Definindo as cores e tipografia da Toolbar

--- app/src/main/java/com/example/instagram/ui/view/HomeScreen.kt
+++ app/src/main/java/com/example/instagram/ui/view/HomeScreen.kt
@@ -1,14 +1,16 @@
 package com.example.instagram.ui.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen (){
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)) {
 
         InstagramToolBar()
     }
