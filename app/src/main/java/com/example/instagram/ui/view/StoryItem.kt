6f2add751541bb654cdeca5fe5495bfd446d24b1
anderodriguesilva <rodriguesilva.anderson@gmail.com>
2023-10-30 16:50:01 -0300

    Organizado o layout do storie do Nickname

--- app/src/main/java/com/example/instagram/ui/view/StoryItem.kt
+++ app/src/main/java/com/example/instagram/ui/view/StoryItem.kt
@@ -1,20 +1,41 @@
 package com.example.instagram.ui.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
+import com.example.instagram.R
 import com.example.instagram.data.Story
+import com.example.instagram.ui.theme.StoryCircleColor
 import com.example.instagram.ui.theme.spacingSmall
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun StoryItem(story: Story) {
+
+    val avatarContentDec =
+        stringResource(id = R.string.content_description_story, story.userNickName)
+
     Column(
         modifier = Modifier
             .padding(spacingSmall)
@@ -22,7 +43,23 @@ fun StoryItem(story: Story) {
     ) {
         GlideImage(
             model = story.userAvatar,
-            contentDescription = "")
+            contentDescription = avatarContentDec,
+            modifier = Modifier
+                .size(64.dp)
+                .align(Alignment.CenterHorizontally)
+                .fillMaxSize()
+                .clip(CircleShape)
+                .border(2.dp, StoryCircleColor, CircleShape),
+            contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = story.userNickName,
+            modifier = Modifier.size(width = 72.dp, height = 24.dp),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            textAlign = TextAlign.Center
+        )
     }
 }
 
