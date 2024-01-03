61c966f69fdf6eb2aeeea61696b46068182c2e2b
anderodriguesilva <rodriguesilva.anderson@gmail.cm>
2024-01-03 13:02:17 -0300

    Criado o feed de postagem

--- app/src/main/java/com/example/instagram/ui/view/HomeScreen.kt
+++ app/src/main/java/com/example/instagram/ui/view/HomeScreen.kt
@@ -12,22 +12,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.instagram.data.model.Feed
 import com.example.instagram.data.model.Story
 import com.example.instagram.data.repository.stories
 import com.example.instagram.ui.theme.DividerColor
 import com.example.instagram.ui.theme.spacingMedium
 
 @Composable
-fun HomeScreen (){
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(MaterialTheme.colorScheme.background)) {
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
 
         InstagramToolBar()
 
         StoryList(stories = stories)
 
         Divider(color = DividerColor, thickness = 0.2.dp)
+
+        FeedItem(
+            feed = Feed(
+                userNickName = "MatheusPereira96",
+                localName = "Belo Horizonte - MG",
+                userAvatar = "https://folhadesabara.com.br/images/noticias/27656/fafa88c435afa8f302264c1ff59d4b6c.jpeg",
+                imageUrl = "https://s2-ge.glbimg.com/US9QSi_8InxulU6qdlZS0RZIjoI=/0x0:1600x1200/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/x/p/J6Wz6BQRqijGQB0r027g/whatsapp-image-2023-07-24-at-15.30.39.jpeg",
+                description = "Cheguei no maior de minas galera! #FechadoComOCruzeiro",
+                postedAgo = "Há 21 horas"
+            )
+        )
     }
 }
 
@@ -42,6 +56,6 @@ fun StoryList(stories: List<Story>) {
 
 @Preview(showBackground = true)
 @Composable
-fun HomeScreenPreview(){
+fun HomeScreenPreview() {
     HomeScreen()
 }
